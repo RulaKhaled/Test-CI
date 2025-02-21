6bf0255db32f75246effe487274c8d60bf74e81a
RulaKhaled <rola.abuhasna@sentry.io>
2025-02-21 14:01:22 +0100

    update with some

--- flag1/mafs.js
+++ flag1/mafs.js
@@ -10,6 +10,8 @@ function multiply(a, b) {
   return a * b;
 }
 
+
+
 function divide(a, b) {
   if (b !== 0) {
     return a / b;
@@ -18,4 +20,4 @@ function divide(a, b) {
   }
 }
 
-module.exports = { add, subtract, multiply };
+module.exports = { add, subtract, multiply, divide };
